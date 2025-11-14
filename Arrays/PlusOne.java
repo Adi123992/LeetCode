10325d6fb0664ba98f8e96040d807eb210cfe61d
addie <aditya.gumgaonkar9021@gmail.com>
2025-11-14 06:06:58 +0530

    Added plus one problem

--- Arrays/PlusOne.java
+++ Arrays/PlusOne.java
@@ -1,7 +1,25 @@
 package Arrays;
 
+import java.util.Arrays;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length -1 ; i >=0 ; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] =0;
+        }
+        int[] result = new int[digits.length+1];
+        result[0]=1;
+        return result;
+    }
+}
 public class PlusOne {
     public static void main(String[] args) {
-        System.out.println("hI there");
+        int[] arr = {1,2,9};
+       Solution s = new Solution();
+        System.out.println(Arrays.toString(s.plusOne(arr)));
     }
 }
