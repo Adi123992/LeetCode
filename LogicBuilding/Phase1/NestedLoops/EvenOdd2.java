d70bb34bc7f6d0e966bb4bb43601f2ab6521465c
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-23 18:30:00 +0530

    Added new in Level 2

---
+++ LogicBuilding/Phase1/NestedLoops/EvenOdd2.java
@@ -0,0 +1,16 @@
+package LogicBuilding.Phase1.NestedLoops;
+
+import java.util.Scanner;
+
+public class EvenOdd2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if ((a % 2 == 0) && (b % 2 == 0)) System.out.println("Both are even");
+        else if ((a % 2 == 0) && (b % 2 != 0)) System.out.println(a + " is even " + b + " is odd");
+        else if ((a % 2 != 0) && (b % 2 == 0)) System.out.println(b + " is even " + a + " is odd");
+        else System.out.println("Both are odd");
+    }
+}
