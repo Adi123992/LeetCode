8090acd78c9770cee963da4f8a837bf265dc0137
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-24 12:05:48 +0530

    Added 2 programs in lvl 2

---
+++ LogicBuilding/Phase1/MathsAndNumbers/MiddleDigit.java
@@ -0,0 +1,16 @@
+package LogicBuilding.Phase1.MathsAndNumbers;
+
+import java.util.Scanner;
+
+public class MiddleDigit {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int d1 = num % 10;
+        int d2 = (num / 10) % 10;
+        int d3 = num / 100;
+        if (d2 > d1 && d2 > d3) System.out.println("Largest");
+        else if (d2 < d1 && d2 < d3) System.out.println("Smallest");
+        else System.out.println("Neither");
+    }
+}
