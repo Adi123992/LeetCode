c4575a93cbde3251c5d08e0e02aaf152d6356ccf
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-25 15:06:14 +0530

    Added new problems in level 5 of phase 1

---
+++ LogicBuilding/Phase1/CreativeLogic/DigitSum1and3.java
@@ -0,0 +1,15 @@
+package LogicBuilding.Phase1.CreativeLogic;
+
+import java.util.Scanner;
+
+public class DigitSum1and3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int d1 = num % 10;
+        int d2 = (num / 10) % 10;
+        int d3 = num / 100;
+        if (d1 + d3 == d2) System.out.println("The sum of " + d3 + " and " + d1 + " is equal to " + d2);
+        else System.out.println("The sum of " + d1 + " and " + d3 + " is not equal to " + d2);
+    }
+}
