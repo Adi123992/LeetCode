04eacc520b7072c2a81afe064159418c98d8faa9
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-29 20:56:08 +0530

    Added newer patterns

---
+++ LogicBuilding/Phase2/StarPatterns/NumberTraingle2.java
@@ -0,0 +1,18 @@
+package LogicBuilding.Phase2.StarPatterns;
+
+import java.util.Scanner;
+
+public class NumberTraingle2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        //row
+        for (int i = 0; i < n; i++) {
+            //elements in each row
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LogicBuilding/Phase2/StarPatterns/NumberTriangle3.java
@@ -0,0 +1,19 @@
+package LogicBuilding.Phase2.StarPatterns;
+
+import java.util.Scanner;
+
+public class NumberTriangle3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        //row
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            //elements in each row
+            for (int j = 0; j <= i; j++) {
+                System.out.print((count++) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
