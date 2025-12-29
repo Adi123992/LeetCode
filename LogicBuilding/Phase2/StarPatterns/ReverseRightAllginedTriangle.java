6bc447e0fe4889c6df0f6bf84437f2159d635f3d
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-29 19:22:09 +0530

    Added more

--- LogicBuilding/Phase2/StarPatterns/ReverseRightAllginedTriangle.java
+++ LogicBuilding/Phase2/StarPatterns/ReverseRightAllginedTriangle.java
@@ -6,10 +6,13 @@ public class ReverseRightAllginedTriangle {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
+        //start from highest row number
         for (int i = n - 1; i >= 0; i--) {
+            //print blank spaces where required in the left sub part
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print("  ");
             }
+            //print stars in the right sub part
             for (int k = 0; k <= i; k++) {
                 System.out.print("* ");
             }
