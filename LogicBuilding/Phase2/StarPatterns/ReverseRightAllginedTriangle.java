8fb2327fba96303781f58e62aa4c9c566a5d84c6
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-29 19:20:31 +0530

    Added more star patterns

--- LogicBuilding/Phase2/StarPatterns/ReverseRightAllginedTriangle.java
+++ LogicBuilding/Phase2/StarPatterns/ReverseRightAllginedTriangle.java
@@ -1,4 +1,19 @@
 package LogicBuilding.Phase2.StarPatterns;
 
+import java.util.Scanner;
+
 public class ReverseRightAllginedTriangle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
