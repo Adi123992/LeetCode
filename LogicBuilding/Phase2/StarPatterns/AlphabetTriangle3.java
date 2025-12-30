03c08613bb6a8cc7aa13538a39ae69c30242a048
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-30 14:50:05 +0530

    Aplhabet Patterns added

---
+++ LogicBuilding/Phase2/StarPatterns/AlphabetTriangle3.java
@@ -0,0 +1,19 @@
+package LogicBuilding.Phase2.StarPatterns;
+
+import java.util.Scanner;
+
+public class AlphabetTriangle3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            char c = 'A';
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c + " ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LogicBuilding/Phase2/StarPatterns/AplhabetTriangle2.java
@@ -0,0 +1,18 @@
+package LogicBuilding.Phase2.StarPatterns;
+
+import java.util.Scanner;
+
+public class AplhabetTriangle2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        char c = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c + " ");
+            }
+            c++;
+            System.out.println();
+        }
+    }
+}
