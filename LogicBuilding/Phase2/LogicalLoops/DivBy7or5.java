7175e768924e3c4ae8b661826b47315927df749f
addie <aditya.gumgaonkar9021@gmail.com>
2025-12-30 16:56:14 +0530

    Added level 5 in phase 2 : Logical loops

---
+++ LogicBuilding/Phase2/LogicalLoops/DivBy7or5.java
@@ -0,0 +1,11 @@
+package LogicBuilding.Phase2.LogicalLoops;
+
+public class DivBy7or5 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i <= 500; i++) {
+            if (i % 7 == 0 && i % 5 != 0) count++;
+        }
+        System.out.println("COunt of numbers divisible by either 7 or 5 is : " + count);
+    }
+}
