349d9aa42c4892af8de99158f4ea32e81f7cb134
addie <aditya.gumgaonkar9021@gmail.com>
2026-01-07 23:01:07 +0530

    Added in transformation and Manipulation

---
+++ LogicBuilding/Phase4_Arrays/Manipulation/SquareElements.java
@@ -0,0 +1,21 @@
+package LogicBuilding.Phase4_Arrays.Manipulation;
+
+import java.util.Scanner;
+
+public class SquareElements {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            arr[i] *= arr[i];
+        }
+        System.out.println("Squared array is : ");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
