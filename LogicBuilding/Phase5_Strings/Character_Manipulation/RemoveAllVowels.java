8b83eeaf4c05f667455647c1094b8ba045764fff
addie <aditya.gumgaonkar9021@gmail.com>
2026-01-21 23:13:21 +0530

    Added remove all vowels

---
+++ LogicBuilding/Phase5_Strings/Character_Manipulation/RemoveAllVowels.java
@@ -0,0 +1,17 @@
+package LogicBuilding.Phase5_Strings.Character_Manipulation;
+
+public class RemoveAllVowels {
+    public static void main(String[] args) {
+        String s = "Aditya";
+        s = s.toLowerCase();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')) {
+                res.append(ch);
+            }
+        }
+        System.out.println(s);
+        System.out.println(res);
+    }
+}
