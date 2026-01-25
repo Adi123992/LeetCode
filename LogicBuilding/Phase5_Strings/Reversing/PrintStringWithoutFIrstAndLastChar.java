e4b539cfefeca4e014a5d96673e5c38816180865
addie <aditya.gumgaonkar9021@gmail.com>
2026-01-25 21:18:01 +0530

    Added str without first and last char

---
+++ LogicBuilding/Phase5_Strings/Reversing/PrintStringWithoutFIrstAndLastChar.java
@@ -0,0 +1,8 @@
+package LogicBuilding.Phase5_Strings.Reversing;
+
+public class PrintStringWithoutFIrstAndLastChar {
+    public static void main(String[] args) {
+        String s = "Aditya";
+        System.out.println(s.substring(1, s.length() - 1));
+    }
+}
