a80d266d390606d729bcbfa846eba7b75a91e0c9
addie <aditya.gumgaonkar9021@gmail.com>
2026-01-25 21:49:26 +0530

    Added longest and shorted word length

--- LogicBuilding/Phase5_Strings/WordLevelThinking/LongestShortestWordLength.java
+++ LogicBuilding/Phase5_Strings/WordLevelThinking/LongestShortestWordLength.java
@@ -1,14 +1,17 @@
 package LogicBuilding.Phase5_Strings.WordLevelThinking;
 
-public class LongestWordLength {
+public class LongestShortestWordLength {
     public static void main(String[] args) {
         String s = "Hey there I am Aditya";
         int max = 0;
+        int min = Integer.MAX_VALUE;
         System.out.println(s);
         String[] arr = s.split("\\s+");
         for (String word : arr) {
             if (word.length() > max) max = word.length();
+            if (word.length() < min) min = word.length();
         }
         System.out.println("Max length of a word is : " + max);
+        System.out.println("Min length of a word is : " + min);
     }
 }
