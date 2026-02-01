dba4693c374e462679eb66012a721e00dc4c80f0
addie <aditya.gumgaonkar9021@gmail.com>
2026-02-01 23:01:38 +0530

    Added some logic

---
+++ SlidingWindow/Char.java
@@ -0,0 +1,20 @@
+package SlidingWindow;
+
+import java.util.Scanner;
+
+public class Char {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            res = res * 26 + (ch - 'A' + 1);
+        }
+        System.out.println(res);
+        String str = "Hey there, I am Aditya";
+        //for replacing all the spaces and alphanumerics wiht an empty string this helps to compare well..
+        str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+        System.out.println(str);
+    }
+}
