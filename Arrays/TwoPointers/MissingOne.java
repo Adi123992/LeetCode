177a3b1b3d41f2eb0200d8eef41600a1a597468b
addie <aditya.gumgaonkar9021@gmail.com>
2026-02-03 22:56:22 +0530

    Added missing one to two pointers

--- Arrays/TwoPointers/MissingOne.java
+++ Arrays/TwoPointers/MissingOne.java
@@ -3,8 +3,20 @@ package Arrays.TwoPointers;
 
 public class MissingOne {
     public static void main(String[] args) {
-        int[] arr = {1, 0, 3};
+        int[] arr = {1, 2, 0, 3};
         System.out.println(missingNumber(arr));
+        System.out.println(missingNumber2(arr));
+    }
+
+    public static int missingNumber2(int[] nums) {
+        //since the loop wont include the length element hence add it in xor itself
+        int xor = nums.length;
+        //wkt 0 ^ a = a && a ^ a = 0 so the number that wont find its pair will be returned
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+            xor ^= i;
+        }
+        return xor;
     }
 
     public static int missingNumber(int[] nums) {
