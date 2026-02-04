51270ba8c41a378c031ad22379b21a643d6c152a
addie <aditya.gumgaonkar9021@gmail.com>
2026-02-04 23:10:16 +0530

    Added remove duplicate words

--- LogicBuilding/Phase6_MixedLogicalChallenges/StringLogic/RemoveDuplicateWords.java
+++ LogicBuilding/Phase6_MixedLogicalChallenges/StringLogic/RemoveDuplicateWords.java
@@ -1,20 +1,29 @@
 package LogicBuilding.Phase6_MixedLogicalChallenges.StringLogic;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class RemoveDuplicateWords {
     public static void main(String[] args) {
         String sent = "Hello there I am there am Aditya";
         String[] words = sent.split("\\s+");
-        HashMap<String, Integer> m = new HashMap<>();
-        String res = "";
+//        HashMap<String, Integer> m = new HashMap<>();
+//        String res = "";
+//        for (String word : words) {
+//            if (m.containsKey(word)) continue;
+//            else {
+//                m.put(word, 1);
+//                res = res + word + " ";
+//            }
+//        }
+        StringBuilder res = new StringBuilder();
+        HashSet<String> set = new HashSet<>();
         for (String word : words) {
-            if (m.containsKey(word)) continue;
+            if (set.contains(word)) continue;
             else {
-                m.put(word, 1);
-                res = res + word + " ";
+                set.add(word);
+                res.append(word).append(" ");
             }
         }
-        System.out.println(res.trim());
+        System.out.println(res.toString());
     }
 }
