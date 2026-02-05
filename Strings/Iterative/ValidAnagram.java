52980854ab0bf1871221cb90f892577f2742e9a3
addie <aditya.gumgaonkar9021@gmail.com>
2026-02-05 23:11:12 +0530

    Added valid anagram methods

--- Strings/Iterative/ValidAnagram.java
+++ Strings/Iterative/ValidAnagram.java
@@ -3,6 +3,7 @@ package Strings.Iterative;
 import java.util.Arrays;
 
 public class ValidAnagram {
+    //efficient
     public static boolean validAnagram(String s, String ana) {
         char[] s1 = s.toCharArray();
         char[] s2 = ana.toCharArray();
@@ -11,9 +12,40 @@ public class ValidAnagram {
         return Arrays.equals(s1, s2);
     }
 
+    //more efficient
+    public static boolean validAnagram2(String s, String ana) {
+        int[] a = new int[26];
+        int[] b = new int[26];
+        for (char ch : s.toCharArray()) {
+            int val = ch - 'a';
+            a[val]++;
+        }
+        for (char ch : ana.toCharArray()) {
+            int val = ch - 'a';
+            b[val]++;
+        }
+        return Arrays.equals(a, b);
+    }
+
     public static void main(String[] args) {
         String str = "anagram";
         String ana = "nagaram";
         System.out.println(validAnagram(str, ana));
+        System.out.println(validAnagram2(str, ana));
     }
 }
+//m2:
+//if(a.length==b.length){
+//     for(int i=0;i<a.length;i++){
+//         boolean found=false;
+//         for(int j=0;j<b.length;j++){
+//             if(a[i]==b[j]){
+//             found=true;
+//             b[j]=' ';
+//             break;
+//             }
+//         }
+//         if(found!=true) return false;
+//     }
+// }
+// return false;
